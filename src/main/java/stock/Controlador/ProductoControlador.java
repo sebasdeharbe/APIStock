cbb0ad3c797679d34ed78fe9e2a77ee94d5c40dd
Sebastián Deharbe <sebasdeharbe@gmail.com>
2022-08-29 08:39:32 -0300

    Some changes

--- src/main/java/stock/Controlador/ProductoControlador.java
+++ src/main/java/stock/Controlador/ProductoControlador.java
@@ -1,8 +1,83 @@
 package stock.Controlador;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import stock.Modelo.Producto;
 
 @RestController
 public class ProductoControlador {
+	private List<Producto> lProductos = new ArrayList<>();
+	
+	@RequestMapping(value = "/producto", method = RequestMethod.GET, produces = "application/json")
+	public List<Producto> index(){
+		return this.lProductos;
+	}
+	
+	private List<Producto> crearListaEjemplo(){
+		Producto oProducto1 = new Producto(1, "Cerveza", 100f);
+		this.lProductos.add(oProducto1);
+		
+		return this.lProductos;
+	}
+	
 	
 }
+
+
+/**
+ * private List<Employee> employees = createList();
+
+	@RequestMapping(value = "/employees", method = RequestMethod.GET, produces = "application/json")
+	public List<Employee> firstPage() {
+		return employees;
+	}
+
+	@DeleteMapping(path = { "/{id}" })
+	public Employee delete(@PathVariable("id") int id) {
+		Employee deletedEmp = null;
+		for (Employee emp : employees) {
+			if (emp.getEmpId().equals(id)) {
+				employees.remove(emp);
+				deletedEmp = emp;
+				break;
+			}
+		}
+		return deletedEmp;
+	}
+
+	@PostMapping
+	public Employee create(@RequestBody Employee user) {
+		employees.add(user);
+		System.out.println(employees);
+		return user;
+	}
+
+	private static List<Employee> createList() {
+		List<Employee> tempEmployees = new ArrayList<>();
+		Employee emp1 = new Employee();
+		emp1.setName("emp1");
+		emp1.setDesignation("manager");
+		emp1.setEmpId("1");
+		emp1.setSalary(3000);
+
+		Employee emp2 = new Employee();
+		emp2.setName("emp2");
+		emp2.setDesignation("developer");
+		emp2.setEmpId("2");
+		emp2.setSalary(3000);
+		tempEmployees.add(emp1);
+		tempEmployees.add(emp2);
+		return tempEmployees;
+	} 
+ * 
+ * */
+ 
